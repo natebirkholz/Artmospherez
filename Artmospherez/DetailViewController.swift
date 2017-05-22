7903bc18710cdbaf737eebdb3964ea88ec095465
natebirkholz <nbirkholz@gmail.com>
2017-05-22 08:15:31 -0700

    Initial commit

---
+++ Artmospherez/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  Artmospherez
+//
+//  Created by Nathan Birkholz on 5/22/17.
+//  Copyright © 2017 natebirkholz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
